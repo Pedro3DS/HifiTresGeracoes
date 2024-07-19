db386a076ca056be34d3606a3a81a5c46a233ce5
Pedroca <pedroca2787@gmail.com>
2024-07-18 21:00:12 -0400

    update
    foi feita a tela inicial

---
+++ Assets/Scripts/StartManager.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        StartGame();
+    }
+
+    private void StartGame(){
+        if(Input.anyKeyDown){
+            SceneManager.LoadScene("Bar70");
+        }
+    }
+}
